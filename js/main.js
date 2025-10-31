e8c60e8ecbbad2cb70c504773b209b97de67a6f0
adorniram <poissonzerty@gmail.com>
2025-10-31 00:28:26 +0000

    index

--- js/main.js
+++ js/main.js
@@ -291,3 +291,21 @@ if ('loading' in HTMLImageElement.prototype) {
   script.src = 'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.3.2/lazysizes.min.js';
   document.body.appendChild(script);
 }
+
+require("dotenv").config();
+
+const http = require("http");
+const { neon } = require("@neondatabase/serverless");
+
+const sql = neon(process.env.DATABASE_URL);
+
+const requestHandler = async (req, res) => {
+  const result = await sql`SELECT version()`;
+  const { version } = result[0];
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end(version);
+};
+
+http.createServer(requestHandler).listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
